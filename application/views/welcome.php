a8f2e3b6462fe75947b110efb391bad086cea3a0
jasonlearn <jasonchan521@hotmail.com>
2017-01-27 11:15:47 -0800

    Synch changes merged
    Signed-off-by: jasonlearn <jasonchan521@hotmail.com>

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,25 +1,11 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>403 Forbidden</title>
+</head>
+<body>
 
-defined('BASEPATH') OR exit('No direct script access allowed');
+<p>Directory access is forbidden.</p>
 
-class Welcome extends Application
-{
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/
-	 * 	- or -
-	 * 		http://example.com/welcome/index
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome');
-	}
-
-}
+</body>
+</html>
